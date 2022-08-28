35f7fe179b6e71ddb293e278ca8eb4bffa8a8693
juj0802 <jak2080@naver.com>
2022-08-29 02:04:04 +0900

    no message

--- main/java/models/user/UserInfoDao.java
+++ main/java/models/user/UserInfoDao.java
@@ -1,7 +1,10 @@
 package models.user;
 
+import org.apache.ibatis.session.SqlSession;
+
 import commons.SQLInsert;
 import commons.SQLUpdate;
+import mybatis.Connection;
 
 public class UserInfoDao implements SQLInsert, SQLUpdate {
 	
@@ -10,6 +13,15 @@ public class UserInfoDao implements SQLInsert, SQLUpdate {
 		boolean result = this.<UserInfoDto>insert(dto, "UserInfoMapper.register");
 		
 		return result;
+	}
+	
+	public UserInfoDto get(String id) {
+		SqlSession session = Connection.getSession();
+		UserInfoDto dto = new UserInfoDto();
+		dto.setId(id);
+		UserInfoDto member = session.selectOne("UserInfoMapper.select",dto);
+		session.close();
+		return member;
 	}
 	
 	
